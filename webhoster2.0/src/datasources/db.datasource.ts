8cd826a2725d291b05b2c9e9f63df9f2724ca00a
Karim Patatas <66632918+Sen2pi@users.noreply.github.com>
2024-01-03 05:18:44 +0000

    atualizacao do dockerfile e compose.yaml

--- webhoster2.0/src/datasources/db.datasource.ts
+++ webhoster2.0/src/datasources/db.datasource.ts
@@ -4,8 +4,7 @@ import {juggler} from '@loopback/repository';
 const config = {
   name: 'db',
   connector: 'mysql',
-  url: '',
-  host: 'localhost',
+  host: 'mysql',
   port: 3306,
   user: 'root',
   password: 'secret',
