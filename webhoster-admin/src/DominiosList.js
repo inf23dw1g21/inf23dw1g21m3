c7f3d940e6577ab4538af789791f029bd7303d51
Karim Patatas <66632918+Sen2pi@users.noreply.github.com>
2024-01-07 00:32:22 +0000

    Create Dominios e Planos , Planos o edit ok por isso é do references o problema

--- webhoster-admin/src/DominiosList.js
+++ webhoster-admin/src/DominiosList.js
@@ -1,19 +1,21 @@
-import {List, Datagrid, TextField, NumberField, DateField, EditButton, Edit, SimpleForm, TextInput,
-    NumberInput, DateInput, useRecordContext, BooleanField, BooleanInput, Filter, ReferenceInput, SelectInput, CreateButton, SaveButton, 
-    Toolbar, Create, useNotify, useRedirect }
+import {
+    List, Datagrid, TextField, NumberField, DateField, EditButton, Edit, SimpleForm, TextInput,
+    NumberInput, DateInput, useRecordContext, BooleanField, BooleanInput, Filter, ReferenceInput, SelectInput, CreateButton, SaveButton,
+    Toolbar, Create, useNotify, useRedirect
+}
     from "react-admin";
 
 const PostFilter = (props) => <Filter {...props}>
-<TextInput label="Procurar" source="cliente" alwaysOn />
-<ReferenceInput label="Dominio" source="cliente"
-reference="dominios" allowEmpty>
-<SelectInput optionText="cliente" />
-</ReferenceInput>
+    <TextInput label="Procurar" source="cliente" alwaysOn />
+    <ReferenceInput label="Dominio" source="cliente"
+        reference="dominios" allowEmpty>
+        <SelectInput optionText="cliente" />
+    </ReferenceInput>
 </Filter>
 
 export const DominioList = (props) => (
     <List filters={<PostFilter />} {...props}>
-        <CreateButton/>
+        <CreateButton />
         <Datagrid rowClick="edit">
             <TextField source="id" />
             <TextField source="nome" />
@@ -28,8 +30,8 @@ export const DominioList = (props) => (
 );
 const PostTitle = () => {
     const record = useRecordContext();
-    return record ? (<span>Dominio {`"${record.subject}"`}</span>):null;
-    }
+    return record ? (<span>Dominio {`"${record.subject}"`}</span>) : null;
+}
 export const DominioEdit = () => (
     <Edit title={<PostTitle />}>
         <SimpleForm>
@@ -44,37 +46,38 @@ export const DominioEdit = () => (
     </Edit>
 );
 
-const PostCreateToolbar = () => {    
-    const redirect = useRedirect();    
+const PostCreateToolbar = () => {
+    const redirect = useRedirect();
     const notify = useNotify();
     return (
         <Toolbar>
-        <SaveButton label="Guardar e Mostrar"/>
-        <SaveButton label="Guardar" 
-        mutationOptions={{                    
-            onSuccess: data => {                        
-                notify('ra.notification.created', {                            
-                    type: 'info',                            
-                    messageArgs: { smart_count: 1 },                        
-                });                        
-                redirect(false);                    
-            }}                
-        }                
-        type="button"
-                variant="text"/>
+            <SaveButton label="Guardar e Mostrar" />
+            <SaveButton label="Guardar"
+                mutationOptions={{
+                    onSuccess: data => {
+                        notify('ra.notification.created', {
+                            type: 'info',
+                            messageArgs: { smart_count: 1 },
+                        });
+                        redirect(false);
+                    }
+                }
+                }
+                type="button"
+                variant="text" />
         </Toolbar>
-        );
-    };
-
-    export const DominioCreate = () => (
-        <Create title={<PostTitle />}>
-            <SimpleForm toolbar={<PostCreateToolbar/>}>
-                <TextInput source="nome" />
-                <TextInput source="codigo_TLD" />
-                <BooleanInput source="estado" />
-                <DateInput source="data_de_inicio" />
-                <DateInput source="data_de_fim" />
-                <NumberInput source="cliente" />
-            </SimpleForm>
-        </Create>
     );
+};
+
+export const DominioCreate = () => (
+    <Create title={<PostTitle />}>
+        <SimpleForm toolbar={<PostCreateToolbar />}>
+            <TextInput source="nome" />
+            <TextInput source="codigo_TLD" />
+            <BooleanInput source="estado" />
+            <DateInput source="data_de_inicio" />
+            <DateInput source="data_de_fim" />
+            <NumberInput source="cliente" />
+        </SimpleForm>
+    </Create>
+);
